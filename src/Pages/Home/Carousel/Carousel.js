62f981e233ece9795eba49d98ab92724badc2b77
Arif <iariful950@gmail.com>
2022-04-17 22:40:25 +0600

    slider done Alhamdulillah!

--- src/Pages/Home/Carousel/Carousel.js
+++ src/Pages/Home/Carousel/Carousel.js
@@ -1,66 +1,96 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import banner1 from '../../../Slider Images/1.jpg';
+import banner2 from '../../../Slider Images/2.jpg';
+import banner3 from '../../../Slider Images/3.jpg';
+import banner4 from '../../../Slider Images/4.jpg';
+import banner5 from '../../../Slider Images/5.jpg';
+import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from 'react-icons/ai';
 import './Carousel.css';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
 
-const images = [
-    {
-        id: 1,
-        img: '/src/images/1.jpg',
-    },
-    {
-        id: 2,
-        img: '/src/images/2.jpg',
-    },
-    {
-        id: 3,
-        img: '/src/images/3.jpg',
-    },
-    {
-        id: 4,
-        img: '/src/images/4.jpg',
-    },
-    {
-        id: 5,
-        img: '/src/images/5.jpg',
-    }
-]
+const bannerImages = [
+    banner1,
+    banner2,
+    banner3,
+    banner4,
+    banner5,
+];
+
+let count = 0;
+let slideInterval;
+
 const Carousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const slideRef = useRef();
+
+    const removeAnimation = () => {
+        slideRef.current.classList.remove('fade-anim');
+    };
+
+    useEffect(() => {
+        slideRef.current.addEventListener('animationend', removeAnimation);
+        slideRef.current.addEventListener('mouseenter', pauseSlider);
+        slideRef.current.addEventListener('mouseleave', startSlider);
+
+        startSlider();
+        return () => {
+            pauseSlider();
+        };
+    }, []);
+
+    const startSlider = () => {
+        slideInterval = setInterval(() => {
+            handleOnNextClick();
+        }, 4000)
+    };
+
+    const pauseSlider = () => {
+        clearInterval(slideInterval);
+    };
+
+    const handleOnNextClick = () => {
+        count = (count + 1) % bannerImages.length;
+        setCurrentIndex(count);
+        slideRef.current.classList.add('fade-anim');
+    };
+
+    const handleOnPrevClick = () => {
+        const bannersLength = bannerImages.length;
+        count = (currentIndex + bannersLength - 1) % bannersLength;
+        setCurrentIndex(count);
+        slideRef.current.classList.add('fade-anim');
+    };
 
     return (
-        <div>
-            <div className='relative'>
-                <Slider infinite="true" autoplay="10000" classNames="buttonDisabled previousButton disabled "
-                    className="slider-wrapper"
-                    previousButton=""
-                    nextButton="">
-                    {
-                        images.map(image => (
-                            <div
-                                className="absolute top-0 w-full"
-                                key={image.id}
-                                style={{
-                                    backgroundImage: `url(${image.img})`,
-                                    backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
-                                    backgroundRepeat: 'no-repeat',
-                                    height: '555px',
-                                }}
-                            >
-                                
-                            </div>
-                        ))
-                    }
-                </Slider>
-                <div>
-                    <span
-                        id="blackOverlay"
-                        className="w-full absolute bg-center opacity-20 bg-black top-0"
-                        style={{ height: '500px' }}
-                    ></span>
+        <div ref={slideRef} className='w-full select-none relative'>
+            <div className='aspect-w-16 aspect-h-9'>
+                <img className='w-full h-[455px] object-cover' src={bannerImages[currentIndex]} alt="" />
+            </div>
+
+            <div className='absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center'>
+                <button className='bg-black text-white p-1 rounded-full bg-oppacity-50 cursor-pointer hover:bg-oppacity-100 transition' onClick={handleOnPrevClick}><AiOutlineVerticalRight size={30}></AiOutlineVerticalRight></button>
+                <button className='bg-black text-white p-1 rounded-full bg-oppacity-50 cursor-pointer hover:bg-oppacity-100 transition' onClick={handleOnNextClick}><AiOutlineVerticalLeft size={30}></AiOutlineVerticalLeft></button>
+            </div>
+
+            <div className='absolute w-full '>
+                <div className="xl:text-5xl text-xl font-bold uppercase text-green-600 mt-10">
+                    <AutoTyping
+                        active // <boolean>
+                        textRef="find your dream car here" // <string>
+                        writeSpeed={100} // <number>
+                        deleteSpeed={40} // <number>
+                        delayToWrite={500} // <number>
+                        delayToDelete={3000} // <number>
+                    />
+                    <BlinkCursor
+                        active // <boolean>
+                        blinkSpeed={500} // <number>
+                    />
                 </div>
             </div>
+
         </div>
+
     );
 };
 
